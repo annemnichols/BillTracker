17e0ed78c8b4c925af37121da6d590c2809c8ccd
annemnichols <anne@seantnichols.com>
2014-12-22 14:55:44 -0700

    Add Users controller, set up basic index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+
   root 'static_pages#index'
 
   get 'about' => 'static_pages#about'
 
   devise_for :users, :controllers => { registration: 'registrations' }
+
+  resources :users, only: [:show, :index]
 
 end
