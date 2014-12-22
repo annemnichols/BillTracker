7a40e053ce7372b81f49808535fc9442ed7eca9b
annemnichols <anne@seantnichols.com>
2014-12-22 15:10:35 -0700

    Fix routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
 
-  devise_for :users, :controllers => { registration: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :users, only: [:show, :index]
 
